38e1a79647b6e1056e024651849a86f8163d0ef8
Andrew Garcia <garcia.gtr@gmail.com>
2023-12-17 13:27:33 -0500

    genim changes

--- main.py
+++ main.py
@@ -18,11 +18,32 @@ def index():
 def image_generator():
     bot_input = request.args.get('botinput')
     imgsize = request.args.get('imgsize')
-    response = openai.Image.create(
-        prompt=bot_input,
-        n=2,
-        size=imgsize
+
+    #TO CREATE IMAGE FROM PROMPT
+    # response = openai.Image.create(
+    #     prompt=bot_input,
+    #     n=2,
+    #     size=imgsize
+    # )
+
+    #IF RECONSTRUCTING / FILLING BLANKS OF IMAGE
+    response = openai.Image.create_edit(
+    image=open("tree.png", "rb"),
+    mask=open("tree_mask.png", "rb"),
+    # prompt="A bear cyborg where the cyborg side, right, is a grey colored robot in the form of a bull animal",
+    prompt=bot_input,
+    n=2,
+    size=imgsize
     )
+
+    #TO CREATE VARIATION OF IMAGE
+    # response = openai.Image.create_variation(
+    # image=open("craiyon_005000_Skyblock_Redstone_Creations.png", "rb"),
+    # n=2,
+    # size=imgsize
+    # )
+
+
 
     print("response", response)
     
